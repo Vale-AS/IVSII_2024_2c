6675ac356b3ece0601ae61eeeae16a3f38cdc476
rosu <mentitas@tuta.io>
2024-11-30 16:22:47 -0300

    Seccion 7: creamos la clase camera

---
+++ Proyecto_Final/camera_class.py
@@ -0,0 +1,75 @@
+from vec3_class import vec3, dot, cross, unit_vector
+from color_func import write_color
+from ray_class import ray
+import sys
+import numpy as np
+from hittable_class import hittable, hit_record
+from hittable_list_class import hittable_list
+from sphere_class import sphere
+from rtweekend import infinity
+from interval_class import interval
+
+color = vec3
+point3 = vec3
+
+class camera:
+    
+    # Image
+    aspect_ratio = 1.0
+    image_width = 100
+
+    def initialize(self):
+
+        # Calculate image height, ensure it's at least 1
+        self.image_height = int(self.image_width / self.aspect_ratio)
+        self.image_height = 1 if (self.image_height < 1) else self.image_height
+
+        # Camera
+        focal_length = 1.0
+        viewport_height = 2.0
+        viewport_width = viewport_height * (float(self.image_width)/self.image_height)
+        self.center = point3(np.array([0, 0, 0]))
+
+        # Calculate vectors across horizontal and down vertical viewport edges
+        viewport_u = vec3(np.array([viewport_width, 0, 0]))
+        viewport_v = vec3(np.array([0, -viewport_height, 0]))
+
+        # Calculate horizontal and vertical delta vectores from pixel to pixel
+        self.pixel_delta_u = viewport_u / self.image_width
+        self.pixel_delta_v = viewport_v / self.image_height
+
+        # Calculate location of upper left pixel
+        viewport_upper_left = self.center - vec3(np.array([0, 0, focal_length])) - viewport_u/2 - viewport_v/2
+        self.pixel00_loc = viewport_upper_left + (self.pixel_delta_u + self.pixel_delta_v) * 0.5
+
+
+    def ray_color(self, r: ray, world: hittable):
+        does_hit, info_rec = world.hit(r, interval(0, infinity))
+        if does_hit:
+            return (info_rec.normal + color(np.array([1,1,1]))) * 0.5
+
+        unit_direction = unit_vector(r.direction())
+        a = 0.5*(unit_direction.y()+1.0)
+        return color(np.array([1.0,1.0,1.0]))*(1.0-a) + color(np.array([0.5,0.7,1.0]))*a
+        
+
+    def render(self, world: hittable, file_name: str):
+
+        self.initialize()
+
+        with open(file_name, "w") as file:
+            file.write(f'P3\n{self.image_width} {self.image_height}\n255\n')
+
+        for j in range(self.image_height):
+            print(f'\rScanlines remaining {self.image_height-j} ', file=sys.stderr)
+            for i in range(self.image_width):
+                pixel_center = self.pixel00_loc + (self.pixel_delta_u * i) + (self.pixel_delta_v * j)
+                ray_direction = pixel_center - self.center
+                r = ray(self.center, ray_direction)
+
+                pixel_color = self.ray_color(r, world)
+                write_color(file_name,pixel_color)
+        
+        print("\rDone.                 \n", file=sys.stderr)
+
+        
--- Proyecto_Final/main.py
+++ Proyecto_Final/main.py
@@ -4,6 +4,7 @@ from ray_class import ray
 import sys
 import numpy as np
 from hittable_class import hittable, hit_record
+from camera_class import camera
 from hittable_list_class import hittable_list
 from sphere_class import sphere
 from rtweekend import infinity
@@ -24,76 +25,21 @@ for j in range(image_height):
 color = vec3
 point3 = vec3
 
-def ray_color(r: ray, world: hittable):
-    #if (hit_sphere(point3(0,0,-1), 0.5, r)):
-    #    return color(1,0,0)
-    #t = hit_sphere(point3(np.array([0,0,-1])), 0.5, r)
-    t, info_rec = world.hit(r, interval(0, infinity))
-    if t:
-        #N = unit_vector(r.at(t) - vec3(0,0,-1))
-        #N = unit_vector(r.at(t) - vec3(np.array([0,0,-1])))
-        #return color(np.array([N.x()+1, N.y()+1, N.z()+1]))*0.5
-        return (info_rec.normal + color(np.array([1,1,1]))) * 0.5
-
-    unit_direction = unit_vector(r.direction())
-    a = 0.5*(unit_direction.y()+1.0)
-    return color(np.array([1.0,1.0,1.0]))*(1.0-a) + color(np.array([0.5,0.7,1.0]))*a
-    
 def main():
-
-    # Image
-
-    aspect_ratio = 16.0 / 9.0
-    image_width = 400
-
-    # Calculate image height, ensure it's at least 1
-    image_height = int(image_width / aspect_ratio)
-    image_height = 1 if (image_height < 1) else image_height
-
-    # Camera
-
-    focal_length = 1.0
-    viewport_height = 2.0
-    viewport_width = viewport_height * (float(image_width)/image_height)
-    camera_center = point3(np.array([0, 0, 0]))
 
     # World
 
     world = hittable_list()
 
-    world.add(sphere(point3(np.array([0,0,-1])), 0.5))
-    world.add(sphere(point3(np.array([0,-100.5,-1])), 100))
+    world.add(sphere(point3(np.array([0,0,-1])), 0.5)) # Pelota
+    world.add(sphere(point3(np.array([0,-100.5,-1])), 100)) # Piso
 
-    # Calculate vectors across horizontal and down vertical viewport edges
-    #viewport_u = vec3(viewport_width, 0, 0)
-    viewport_u = vec3(np.array([viewport_width, 0, 0]))
-    #viewport_v = vec3(0, -viewport_height, 0)
-    viewport_v = vec3(np.array([0, -viewport_height, 0]))
+    cam = camera()
 
-    # Calculate horizontal and vertical delta vectores from pixel to pixel
-    pixel_delta_u = viewport_u / image_width
-    pixel_delta_v = viewport_v / image_height
+    cam.aspect_ratio = 16.0/9.0
+    cam.image_width = 400
 
-    # Calculate location of upper left pixel
-    #viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2
-    viewport_upper_left = camera_center - vec3(np.array([0, 0, focal_length])) - viewport_u/2 - viewport_v/2
-    pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5
+    cam.render(world, "images/image10.ppm")
 
-    # Render 
-
-    with open("image9.ppm", "w") as file:
-        file.write(f'P3\n{image_width} {image_height}\n255\n')
-
-    for j in range(image_height):
-        print(f'\rScanlines remaining {image_height-j} ', file=sys.stderr)
-        for i in range(image_width):
-            pixel_center = pixel00_loc + (pixel_delta_u * i) + (pixel_delta_v * j)
-            ray_direction = pixel_center - camera_center
-            r = ray(camera_center, ray_direction)
-
-            pixel_color = ray_color(r, world)
-            write_color("image9.ppm",pixel_color)
-    
-    print("\rDone.                 \n", file=sys.stderr)
 
 main()
